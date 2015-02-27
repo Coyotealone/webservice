7e0e95ad6421b6f0508d36add2bdeba5502fc9d4
Coyote <coyotealone@hotmail.fr>
2015-02-27 11:50:58 +0100

    0.3.0
    Add Sonata
    Configure Sonata

---
+++ src/Coyote/ApiBundle/Admin/PersoAdmin.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Coyote\ApiBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class PersoAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('name')
+            ->add('level')
+            ->add('class')
+            ->add('race')
+            ->add('sexe')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('name')
+            ->add('level')
+            ->add('class')
+            ->add('race')
+            ->add('sexe')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('name')
+            ->add('level')
+            ->add('class')
+            ->add('race')
+            ->add('sexe')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('level')
+            ->add('class')
+            ->add('race')
+            ->add('sexe')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+}
